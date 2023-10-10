dc11d3d781edabeaf9052227f6c5bba1da0ca040
Dima Lysak <60256445+z8Dimon8z@users.noreply.github.com>
2023-10-10 21:07:27 +0300

    Урок 25

---
+++ src/js/files/script.js
@@ -0,0 +1,10 @@
+// Подключение функционала "Чертогов Фрилансера"
+// import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+// import { flsModules } from "./modules.js";
+
+// Свой код который пишем
+window.addEventListener('load', function (e) { // событие полной загрузки страницы
+  document.body.insertAdjacentHTML('beforeend', `<div class="main-bg"></div>`);
+  
+});
